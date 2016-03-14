662145eabbc6a8e44065a51ff058d03227bbb06d
PD <pwanjari@godaddy.com>
2016-03-14 13:44:49 -0700

    Initial commit
    Signed-off-by: PD <pwanjari@godaddy.com>

---
+++ Login.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var React = require('react-native');
+var buffer = require('buffer');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+	Image,
+	TextInput,
+	TouchableHighlight,
+	Component,
+	ActivityIndicatorIOS
+} = React;
+
+class Login extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			showProgress: false
+		}
+	}
+	render() {
+		var errControl = <View />;
+
+		if (!this.state.success && this.state.badCredentials) {
+			errControl = <Text style={styles.error}>
+				That username and password combination did not work.
+			</Text>;
+		}
+
+		if (!this.state.success && this.state.unknownError) {
+			errControl = <Text style={styles.error}>
+				We experienced an unexpected issue
+			</Text>;
+		}
+
+		return (
+			//<Text style={{color: 'white'}}>Hello one</Text>
+			<View style={styles.container}>
+				<Image style={styles.logo} source={require('image!Octocat')} />
+				<Text style={styles.heading}>
+					Kool Github browser
+				</Text>
+				<TextInput
+					onChangeText={(text) => this.setState({userName: text})}
+					style={styles.input} placeholder="Github Username" />
+				<TextInput
+					onChangeText={(text) => this.setState({password: text})}
+					style={styles.input} placeholder="Github Password" secureTextEntry={true} />
+				<TouchableHighlight
+					onPress={this.onLoginPressed.bind(this)}
+					style={styles.button}>
+					<Text style={styles.buttonText}>Log in </Text>
+				</TouchableHighlight>
+				{errControl}
+				<ActivityIndicatorIOS
+					animating={this.state.showProgress} size="large"
+					style={styles.loader} />
+			</View>
+		);
+	}
+
+	onLoginPressed() {
+		console.log('Attempting to login in with the username ' + this.state.userName);
+		this.setState({showProgress: true});
+
+		var authService = require('./AuthService');
+		authService.login({
+			userName: this.state.userName,
+			password: this.state.password
+		}, (results) => {
+			this.setState(Object.assign({
+				showProgress: false
+			}, results));
+
+      if(results.success && this.props.onLogin) {
+        this.props.onLogin();
+      }
+		});
+
+	}
+};
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: 40,
+		alignItems: 'center',
+		backgroundColor: '#FFF',
+		padding: 10
+	},
+	logo: {
+		width: 66,
+		height: 55
+	},
+	heading: {
+		fontSize: 30,
+		marginTop: 10,
+		color: '#48BBEC'
+	},
+	input: {
+		height: 50,
+		marginTop: 10,
+		padding: 4,
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: '#48bbec'
+	},
+	button: {
+		height: 50,
+		backgroundColor: '#48BBEC',
+		alignSelf: 'stretch',
+		marginTop: 10,
+		justifyContent: 'center'
+	},
+	buttonText: {
+		fontSize: 22,
+		color: '#FFF',
+		alignSelf: 'center'
+	},
+	loader: {
+		marginTop: 24
+	},
+	error: {
+		color: 'red',
+		paddingTop: 10
+	}
+});
+
+
+module.exports = Login;
---
+++ index.ios.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var Login = require('./Login');
+
+class GithubBrowser extends Component {
+  render() {
+    var message = 'Hello there';
+    var isLoggedIn = false;
+    return (
+        <Login onLogin={this.onLogin} />
+    );
+  }
+  onLogin() {
+    console.log('Successfully logged in, ready to move to the next view');
+    this.setState({isLoggedIn: true});
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('GithubBrowser', () => GithubBrowser);
